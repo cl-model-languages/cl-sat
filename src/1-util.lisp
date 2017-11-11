6ddf6542f5a0393aed09e9baba312c4f52c5bf7a
Masataro Asai <guicho2.71828@gmail.com>
2017-11-11 19:17:15 +0900

    doc

--- src/1-util.lisp
+++ src/1-util.lisp
@@ -1,6 +1,10 @@
 (in-package :cl-sat)
 
 (defmacro with-temp ((var &key directory template (tmpdir "/tmp/") debug) &body body)
+  "Create a temporary file, then remove the file by unwind-protect.
+Most arguments are analogous to mktemp.
+When DIRECTORY is non-nil, creates a directory instead.
+When DEBUG is non-nil, it does not remove the directory so that you can investigate what happened inside the directory."
   `(let ((,var (uiop:run-program (format nil "mktemp --tmpdir='~a' ~@[-d~*~] ~a" ,tmpdir ,directory ,template)
                                  :output '(:string :stripped t))))
      (unwind-protect
